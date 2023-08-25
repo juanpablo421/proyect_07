324fa7de705ee82abcd8c6e3ff81a3ca14bcaf74
25JuanP <jb061366@gmail.com>
2023-08-24 20:13:08 -0500

    editado

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('colors');
 const math = require('./proy_modules/math.js')
 
 const main = async () => {
-    console.log('hola sena cba\n'.red);
+    console.log('***********************************************************************************************************'.green);
+    console.log('HELLO WORLD\n'.red);
     const numeros = [4, 3, 568540, 689, 68495, 49632, 4643256];
     for (const numero of numeros){
         const invertidocomoNumero = math.invertirNumero(numero);
@@ -12,7 +13,7 @@ const main = async () => {
         numero, invertidocomoNumero, invertidocomocadena);
 
     }
-    const textos = ['hola mundo', 'ficha 2798618', 'Analisis y Deasarrollo de Software']
+    const textos = ['hola que tal', 'ficha 2798618', 'Analisis y Deasarrollo de Software']
     for(const texto of textos){
         const textoInvertido = math.invertirtexto(texto);
         console.log("El texto '%s' se invierte como '%s'", texto, textoInvertido);
@@ -22,6 +23,7 @@ const main = async () => {
         const datoconvertido = math.conversionDatos(dato);
         console.log(`El resultado de conversion es: ${datoconvertido}`);
     }
+    console.log('***********************************************************************************************************\n'.green);
 }
 
 
